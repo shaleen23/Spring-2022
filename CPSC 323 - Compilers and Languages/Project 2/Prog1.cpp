370f86d3523511618299257116301a32e0022ec7
Shaleen Mathur <47233736+shaleen23@users.noreply.github.com>
2022-05-23 15:58:39 -0700

    Update Prog1.cpp

--- CPSC 323 - Compilers and Languages/Project 2/Prog1.cpp
+++ CPSC 323 - Compilers and Languages/Project 2/Prog1.cpp
@@ -1,3 +1,9 @@
+/*
+Given the following CFG and the Predictive Parsing table. Write a program to trace an input string given by the user. 
+Save it as Prog1. Test your program with the following input strings:  (1) (a +a )*a$, (2) a*(a/a)$, (3) a*(a+a)$. 
+Show the content of the stack after each match
+*/
+
 #include <iostream>
 #include <string>
 #include <sstream>
